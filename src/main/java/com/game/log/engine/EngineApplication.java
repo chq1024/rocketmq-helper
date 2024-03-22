7d8ee53a3352614dc889d78a51a4ada2de8c699a
chuhq <hq.chu@heart7.cn>
2024-03-22 19:48:45 +0800

    init: game log engine

---
+++ src/main/java/com/game/log/engine/EngineApplication.java
@@ -0,0 +1,16 @@
+package com.game.log.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author beikei
+ */
+@SpringBootApplication
+public class EngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineApplication.class, args);
+    }
+
+}
